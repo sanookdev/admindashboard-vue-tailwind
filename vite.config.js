266f3c52132730315b7bb1a8a5f1e6854b182e0d
Sanooker555 <104189137+sanookdev@users.noreply.github.com>
2023-12-01 11:37:34 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/services/',
+  base: '/dashboard/',
   plugins: [vue()],
   resolve: {
     alias: {
